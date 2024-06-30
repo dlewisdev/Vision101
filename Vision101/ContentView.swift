b63890695d1f2cfdc2b750eaf6c5fd8a294abb09
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-06-30 13:44:35 -0400

    Initial Commit

---
+++ Vision101/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Vision101
+//
+//  Created by Danielle Lewis on 6/30/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Vision101/Vision101App.swift
@@ -0,0 +1,17 @@
+//
+//  Vision101App.swift
+//  Vision101
+//
+//  Created by Danielle Lewis on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Vision101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
