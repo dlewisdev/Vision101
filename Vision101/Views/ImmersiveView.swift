f426b8438782f8965585bc7317113b6c2254fc9d
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-07-14 14:35:16 -0400

    Open immersive view and make the window disappear to view the realitykit scene

--- Vision101/Views/ImmersiveView.swift
+++ Vision101/Views/ImmersiveView.swift
@@ -6,10 +6,25 @@
 //
 
 import SwiftUI
+import RealityKit
+import RealityKitContent
 
 struct ImmersiveView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RealityView { content in
+            if let scene = try? await Entity(named: "ImmersiveScene", in: realityKitContentBundle) {
+                content.add(scene)
+            }
+        } update: { content in
+            if let scene = content.entities.first {
+                scene.availableAnimations.forEach {
+                    animation in
+                    scene.playAnimation(animation.repeat(), transitionDuration: 3, startsPaused: false)
+                }
+            }
+        }
+        .offset(y: -2000)
+        .offset(z: -1500)
     }
 }
 
