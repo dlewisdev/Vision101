08892d6d24b6696e1f51842bbddac10490ab35f0
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-07-14 15:03:43 -0400

    Add SkyView as the immersive background

--- Vision101/Views/SkyView.swift
+++ Vision101/Views/SkyView.swift
@@ -6,10 +6,25 @@
 //
 
 import SwiftUI
+import RealityKit
+import RealityKitContent
 
 struct SkyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RealityView { content in
+            guard let resource = try? await TextureResource(named: "skyView") else {
+                return
+            }
+            
+            var material = UnlitMaterial()
+            material.color = .init(texture: .init(resource))
+            
+            let entity = Entity()
+            entity.components.set(ModelComponent(mesh: .generateSphere(radius: 1000), materials: [material]))
+            entity.scale *= .init(x: -1, y: 1, z: 1)
+            
+            content.add(entity)
+        }
     }
 }
 
