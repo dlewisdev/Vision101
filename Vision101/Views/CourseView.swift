0a2bcd602d5a2b7222937eb9cb209090afe74fc7
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-07-14 14:34:51 -0400

    Display course name and content

--- Vision101/Views/CourseView.swift
+++ Vision101/Views/CourseView.swift
@@ -14,11 +14,15 @@ struct CourseView: View {
         VStack {
             Text(course.name)
                 .font(.largeTitle)
+            
+            Text(course.content)
+            Spacer()
         }
+        .padding(20)
         .navigationTitle(course.name)
     }
 }
 
 #Preview {
-    CourseView(course: Course(name: "visionOS"))
+  CourseView(course: Course(name: "visionOS", content: "Content, content and content. Ut necessitatibus voluptate praesentium id eos eaque itaque cumque. Sunt error et et. Dignissimos veritatis eum ad eius omnis. Pariatur eaque nihil fuga omnis quia. Aperiam corporis odit vero aspernatur in recusandae.Delectus quo sed dolores quo architecto et necessitatibus aut. Velit impedit animi est. Sapiente animi nostrum aperiam quod ut eos."))
 }
