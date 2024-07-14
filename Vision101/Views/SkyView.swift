253bfb6f30b58dcdcdcbc76c368f1f04e442c209
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-07-14 15:03:15 -0400

    Add image assets and skyview file to project

---
+++ Vision101/Views/SkyView.swift
@@ -0,0 +1,18 @@
+//
+//  SkyView.swift
+//  Vision101
+//
+//  Created by Danielle Lewis on 7/14/24.
+//
+
+import SwiftUI
+
+struct SkyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkyView()
+}
