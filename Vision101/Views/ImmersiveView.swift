cc37290384e1c86de38601d62713ece0e7ad0e51
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-07-14 15:04:01 -0400

    reformat code

--- Vision101/Views/ImmersiveView.swift
+++ Vision101/Views/ImmersiveView.swift
@@ -11,20 +11,60 @@ import RealityKitContent
 
 struct ImmersiveView: View {
     var body: some View {
+        
+        SkyView()
+        
         RealityView { content in
-            if let scene = try? await Entity(named: "ImmersiveScene", in: realityKitContentBundle) {
+            if let scene = try? await Entity(
+                named: "ImmersiveScene",
+                in: realityKitContentBundle) {
                 content.add(scene)
             }
+            guard let resource = try? await EnvironmentResource(named: "Sunlight") else { return }
+            
         } update: { content in
             if let scene = content.entities.first {
                 scene.availableAnimations.forEach {
                     animation in
-                    scene.playAnimation(animation.repeat(), transitionDuration: 3, startsPaused: false)
+                    scene.playAnimation(
+                        animation.repeat(),
+                        transitionDuration: 3,
+                        startsPaused: false
+                    )
+                    
+                    scene.orientation = simd_quatf(angle: Float.pi/4, axis: [-0.5, 0.0, 0.0])
+                    
+                    let orbit = OrbitAnimation(
+                        duration: 10.0,
+                        axis: SIMD3<Float>(
+                            x: 0.1,
+                            y: 1.0,
+                            z: 0.0
+                        ),
+                        startTransform: scene.transform,
+                        spinClockwise: false,
+                        orientToPath: true,
+                        rotationCount: 1.0,
+                        bindTarget: .transform,
+                        repeatMode: .repeat
+                    )
+                    
+                    if let animation = try? AnimationResource.generate(
+                        with: orbit
+                    ) {
+                        scene.playAnimation(
+                            animation
+                        )
+                    }
                 }
             }
         }
-        .offset(y: -2000)
-        .offset(z: -1500)
+        .offset(
+            y: -2000
+        )
+        .offset(
+            z: -1500
+        )
     }
 }
 
